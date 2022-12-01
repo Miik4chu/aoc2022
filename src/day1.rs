334860844b04f74a638ddc318c4d4711990f4505
Michael Bui <bui.michael.91@gmail.com>
2022-12-01 21:42:18 +0100

    Day 1, let's go easy shall we

---
+++ src/day1.rs
@@ -0,0 +1,51 @@
+pub fn parse_input(input: &str) -> Vec<u32> {
+    input
+        .split("\n\n")
+        .map(|calories| {
+            calories
+                .lines()
+                .filter_map(|calory| calory.parse::<u32>().ok())
+                .sum()
+        })
+        .collect()
+}
+pub fn part1(input: &str) -> Option<u32> {
+    let calories = parse_input(input);
+
+    calories.into_iter().max()
+}
+
+pub fn part2(input: &str) -> u32 {
+    let mut calories = parse_input(input);
+
+    calories.sort();
+    calories.reverse();
+
+    calories[0] + calories[1] + calories[2]
+}
+
+#[cfg(test)]
+pub mod tests {
+    const INPUT: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    use super::{part1, part2};
+
+    #[test]
+    fn solve_day_1() {
+        assert_eq!(part1(INPUT), Some(24000));
+        assert_eq!(part2(INPUT), 45000);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use aoc2022::*;
+use std::{env, fs};
+
+macro_rules! run_day {
+    ($day:path, $input:expr) => {{
+        use $day::{part1, part2};
+
+        println!(
+            "{}: part1 = {:?}, part2 = {:?}",
+            stringify!($day),
+            part1($input),
+            part2($input)
+        );
+    }};
+}
+
+pub fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let input = fs::read_to_string(format!("input/day{}.txt", args[1].as_str()))
+        .expect("Something went wrong reading the file");
+    let input_str: &str = input.as_str();
+
+    let day_num: u8 = args[1].parse().unwrap();
+
+    match day_num {
+        1 => run_day!(day1, input_str),
+        _ => println!("Invalid day number: {}", day_num),
+    }
+}
